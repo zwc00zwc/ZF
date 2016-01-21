4bc9cf6adc802e2e447139f97c3968fa00417a87
zwc00zwc <1484659036@qq.com>
2016-01-21 17:02:20 +0800

    init
    init

---
+++ ZF/ZF/Db/SqlHelper/SqlServerHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZF.Db.SqlHelper
+{
+    public class SqlServerHelper
+    {
+        public static DataTable Get(string conn, string sql, params SqlParameter[] paramter)
+        {
+            SqlConnection dbconn = new SqlConnection(conn);
+            dbconn.Open();
+            using (SqlCommand cmd = dbconn.CreateCommand())
+            {
+                cmd.CommandText = sql;
+                cmd.Parameters.AddRange(paramter);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dbconn.Close();
+                return dt;
+            }
+        }
+    }
+}

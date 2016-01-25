80390e60d08761a88f194b336532553cd3713a3b
zwc00zwc <1484659036@qq.com>
2016-01-25 14:26:55 +0800

    Redis连接池
    Redis连接池

---
+++ ZF/ZF/Redis/RedisManager.cs
@@ -0,0 +1,27 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZF.Redis
+{
+    public class RedisManager
+    {
+        /// <summary>
+        /// 连接池进行创建连接
+        /// </summary>
+        /// <returns></returns>
+        public IRedisClient GetClient(List<string> readwritehosts,List<string> readhosts)
+        {
+            RedisClientManagerConfig redisconfig = new RedisClientManagerConfig();
+            redisconfig.MaxReadPoolSize = 5;
+            redisconfig.MaxWritePoolSize = 5;
+            redisconfig.AutoStart = true;
+
+            PooledRedisClientManager poolredis = new PooledRedisClientManager(readwritehosts, readhosts, redisconfig);
+            return poolredis.GetClient();
+        }
+    }
+}
